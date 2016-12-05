b8526d0ce87d4be75f4dfee95e979120af15e95b
Ramses87 <Gerral2@gmail.com>
2016-12-05 18:07:51 +0200

    Magecom_Gift Magento module version 0.1.0, added 05.12.2016

---
+++ app/code/local/Magecom/Gift/Helper/Data.php
@@ -0,0 +1,5 @@
+<?php
+
+class Magecom_Gift_Helper_Data extends Mage_Core_Helper_Abstract{
+
+}
---
+++ app/code/local/Magecom/Gift/Model/Observer.php
@@ -0,0 +1,45 @@
+<?php
+
+class Magecom_Gift_Model_Observer
+{
+    public function addGift()
+    {
+    		
+			$price = (int)trim(Mage::getStoreConfig('Magecom/Magecom_group/Magecom_inputone',Mage::app()->getStore())); //  акционное значение суммы товаров, после - добавляем подарок, устанавливаем в админке
+			$giftId = (int)trim(Mage::getStoreConfig('Magecom/Magecom_group/Magecom_inputtwo',Mage::app()->getStore())); //  идентификатор подарка (id product), устанавливаем в админке
+			
+			
+			$total = Mage::getSingleton('checkout/cart')->getQuote()->getGrandTotal();// Получаю суммарную цену товаров в квоте
+
+    		if ($total >= $price) {                                                      //проверяю условие сумма больше или равна акц. цене
+
+            $cart = Mage::getSingleton('checkout/cart')->getQuote(); 
+            foreach ($cart->getAllVisibleItems() as $item) {  
+                if ($item->getProductId() == $giftId) {
+                    // die('df');
+                    return;
+                }
+            }
+
+          
+      			$product = Mage::getModel('catalog/product')->load($giftId); // проверяем существует ли подарок
+      			if (!$product->getId()) {
+      				return;
+      			}
+
+      			$quote = Mage::getSingleton('checkout/cart')->getQuote();   //добавляем подарок с 0 ценой
+                $quoteItem = $quote->addProduct($product, 1);
+      			$quoteItem->setCustomPrice(0);
+      			$quoteItem->setOriginalCustomPrice(0);
+    		} else {                                                       // в другом случае ( сумма квоты меньше акц.цены) 
+            $cart = Mage::getSingleton('checkout/cart')->getQuote();       //проверяем наличие подарка  и удаляем подарок из квоты
+            foreach ($cart->getAllVisibleItems() as $item) {
+
+                if ($item->getProductId() == $giftId) {
+                    Mage::getSingleton('checkout/cart')->getQuote()->removeItem($item->getItemId())->save();
+                }
+            }
+        }
+    }
+
+}
